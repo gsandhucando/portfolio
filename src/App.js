f3ffd54cf17201a5e4bd20ce200e39219f213398
gsandhu916 <gsandhu916@gmail.com>
2019-07-07 19:03:33 -0700

    title page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import TitlePage from './components/TitlePage';
 
 function App() {
   return (
     <div className="App">
-      <h1>Portfolio</h1>
+      <TitlePage />
     </div>
   );
 }
---
+++ src/components/TitlePage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const TitlePage = () => {
+  let nav = ['about me', 'projects', 'resume'].map(list => {
+    return <li>{list}</li>
+  })
+  return (
+    <div className="title-page-container">
+      <img className="title-page-logo" src="./images/logo.png" alt='logo' />
+      <div className="title-page-textbox">
+      <h3>Software Engineer</h3>
+      <>
+      <div className="title-page-nav-container">
+        <ul className="title-page-ul">
+        {nav}
+        </ul>
+      </div>
+      </>
+      </div>
+    </div>
+  )
+}
+
+export default TitlePage;
